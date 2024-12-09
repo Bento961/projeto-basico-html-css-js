cda655782d41d73305b4927a63a022d8949b85b7
Felipe Bento <fbento4596@gmail.com>
2024-12-09 13:42:22 -0300

    projeto-web

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", function() {
+    const output = document.getElementById("output");
+    output.textContent = "Você clicou no botão!";
+    output.style.color = "#0056b3";
+});
